f0c8affcc07942c288dd950e5068e5e9817bf410
ay27 <me@ay27.pw>
2014-12-03 16:28:10 +0800

    test

--- pos_v1/src/main.js
+++ pos_v1/src/main.js
@@ -1,1 +1,9 @@
 //TODO: Please write code in this file.
+
+function printInventory(inputs) {
+	var allItems = loadAllItems();
+	var resultString = "***<没钱赚商店>购物清单***\n";
+	for (var i = inputs.length - 1; i >= 0; i--) {
+
+	}
+}
