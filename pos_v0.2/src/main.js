9a77b58ca094daf1e3de598a33c4b7aa9a84a25f
ay27 <me@ay27.pw>
2014-12-05 01:09:32 +0800

    finish pos_v0.2

--- pos_v0.2/src/main.js
+++ pos_v0.2/src/main.js
@@ -1,4 +1,36 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
+	var allItemInfo = loadAllItems();
 
+	for (var i = 0; i < inputs.length; i++) {
+		for (var j = 0; j < allItemInfo.length; j++) {
+			if (allItemInfo[j].barcode == inputs[i]) {
+				allItemInfo[j].count = (allItemInfo[j].hasOwnProperty('count'))? allItemInfo[j].count+1 : 1;
+				break;
+			}
+		};
+	}
+
+	var sum = 0;
+    var resultString = '***<没钱赚商店>购物清单***';
+
+	for (var i = 0; i < allItemInfo.length; i++) {
+		if (allItemInfo[i].hasOwnProperty('count')) {
+			allItemInfo[i].getCost = function() { return this.price*this.count; }
+			sum += allItemInfo[i].getCost();
+			resultString += '\n' + getItemMessage(allItemInfo[i]);
+		}
+	}
+
+    resultString += '\n----------------------\n总计：' + sum.toFixed(2) + '(元)';
+    resultString += '\n**********************';
+
+    console.log(resultString);
+}
+
+function getItemMessage(item) {
+	return '名称：' + item.name +
+        '，数量：' + item.count + item.unit +
+        '，单价：' + item.price.toFixed(2) +
+        '(元)，小计：' + item.getCost().toFixed(2) + '(元)';
 }
