f9aa8e50d97388310481b53f2629c0a29c449f2c
ay27 <me@ay27.pw>
2014-12-05 01:25:00 +0800

    refactor pos_v0.2

--- pos_v0.2/src/main.js
+++ pos_v0.2/src/main.js
@@ -1,20 +1,19 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
-	var allItemInfo = countProduct(inputs);
-	printResult(allItemInfo);
+    var allItemInfo = countProduct(inputs);
+    printResult(allItemInfo);
 }
 
 function printResult(allItemInfo) {
-	var sum = 0;
+    var sum = 0;
     var resultString = '***<没钱赚商店>购物清单***';
 
-	for (var i = 0; i < allItemInfo.length; i++) {
-		if (allItemInfo[i].hasOwnProperty('count')) {
-			allItemInfo[i].getCost = function() { return this.price*this.count; }
-			sum += allItemInfo[i].getCost();
-			resultString += '\n' + getItemMessage(allItemInfo[i]);
-		}
-	}
+    for (var i = 0; i < allItemInfo.length; i++) {
+        if (allItemInfo[i].hasOwnProperty('count')) {
+            sum += allItemInfo[i].getCost();
+            resultString += '\n' + getItemMessage(allItemInfo[i]);
+        }
+    }
 
     resultString += '\n----------------------\n总计：' + sum.toFixed(2) + '(元)';
     resultString += '\n**********************';
@@ -23,22 +22,30 @@ function printResult(allItemInfo) {
 }
 
 function countProduct(inputs) {
-	var allItemInfo = loadAllItems();
+    var allItemInfo = loadAllItems();
 
-	for (var i = 0; i < inputs.length; i++) {
-		for (var j = 0; j < allItemInfo.length; j++) {
-			if (allItemInfo[j].barcode == inputs[i]) {
-				allItemInfo[j].count = (allItemInfo[j].hasOwnProperty('count'))? allItemInfo[j].count+1 : 1;
-				break;
-			}
-		};
-	}
+    for (var i = 0; i < inputs.length; i++) {
+        var oneItemInfo = findMatchItemInfo(allItemInfo, inputs[i]);
+        oneItemInfo.count = (oneItemInfo.hasOwnProperty('count')) ? oneItemInfo.count + 1 : 1;
+        oneItemInfo.getCost = function() {
+        	return this.price * this.count;
+        }
+    }
 
-	return allItemInfo;
+    return allItemInfo;
+}
+
+function findMatchItemInfo(allItemInfo, barcode) {
+    for (var i = 0; i < allItemInfo.length; i++) {
+        if (allItemInfo[i].barcode == barcode) {
+            return allItemInfo[i];
+        }
+    }
+    return null;
 }
 
 function getItemMessage(item) {
-	return '名称：' + item.name +
+    return '名称：' + item.name +
         '，数量：' + item.count + item.unit +
         '，单价：' + item.price.toFixed(2) +
         '(元)，小计：' + item.getCost().toFixed(2) + '(元)';
