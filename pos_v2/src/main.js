00024c753b82a3289c90fa24ddb2cc665aa0555f
ay27 <me@ay27.pw>
2014-12-15 16:03:57 +0800

    calculate the accurate model.

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -1,1 +1,18 @@
 //TODO: Please write code in this file.
+
+function insert_data_to_array( array_a ,insert_index, array_b)
+{
+    //在这里写入代码
+	var result = array_a.concat();
+    var b = array_b.concat();
+  	result.splice(insert_index, 0, b);
+	return result;
+}
+
+
+function printInventory() {
+	var array_b = [13,1,11];
+	var array_a = [9,14,19,27,14,2,8,5,15,12,22,26];
+	var x = insert_data_to_array(array_a, 3, array_b);
+	console.log(x);
+}
--- pos_v2_3rdlib/src/main.js
+++ pos_v2_3rdlib/src/main.js
@@ -1,1 +1,32 @@
-//TODO: Please write code in this file.
+function printInventory(inputs) {
+    var items = loadAllItems();
+    count_goods(inputs, items);
+    var goods_statistics = calculate_promotion(items, loadPromotions());
+    var receipt = generate_receipt(goods_statistics);
+    console.log(receipt);
+}
+
+function count_goods(inputs, items) {
+    _.forEach(inputs, function (oneInput) {
+        _.forEach(_.filter(items, function (oneItem) {
+            return oneInput.split('-')[0] === oneItem.barcode;
+        }), function (oneItem) {
+            oneItem.count = (oneItem.count === undefined) ? 0 : oneItem.count;
+            oneItem.count += (oneInput.split('-')[1] === undefined) ? 1 : _.parseInt(oneInput.split('-')[1]);
+        });
+    });
+}
+
+function calculate_promotion(items, promotions) {
+    var barcode_in_promotion = _.filter(promotions, {'type':'BUY_TWO_GET_ONE_FREE'})[0].barcodes;
+    _.forEach(items, function (item) {
+        item.freeCount = _.contains(barcode_in_promotion, item.barcode) ?
+            (item.count > 2) ? 1 : 0
+            :
+            0;
+    });
+}
+
+function generate_receipt(goods_statistics) {
+
+}
